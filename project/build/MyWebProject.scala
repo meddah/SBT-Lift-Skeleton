34ccf69aed2157c237af0594327a23223173f980
Vesa Vilhonen <vesa@vilhonen.com>
2010-02-01 17:11:52 +0200

    Update derby

--- project/build/MyWebProject.scala
+++ project/build/MyWebProject.scala
@@ -5,7 +5,7 @@ class MyWebProject(info: ProjectInfo) extends DefaultWebProject(info)
   val lift = "net.liftweb" % "lift-core" % "1.1-M8" % "compile->default"
   val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default"
   val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided->default"
-  val derby = "org.apache.derby" % "derby" % "10.2.2.0" % "runtime->default"
+  val derby = "org.apache.derby" % "derby" % "10.5.3.0_1" % "runtime->default"
   val specs = "org.scala-tools.testing" % "specs" % "1.6.1" % "test->default"
 
   // required because Ivy doesn't pull repositories from poms
