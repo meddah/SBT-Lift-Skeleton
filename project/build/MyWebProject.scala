2cc5b8e5a972149db148e265ef14ba812ccf8894
Vesa Vilhonen <vesa@vilhonen.com>
2010-03-18 18:46:32 +0200

    Update lift to 2.0-M3

--- project/build/MyWebProject.scala
+++ project/build/MyWebProject.scala
@@ -2,11 +2,13 @@ import sbt._
 
 class MyWebProject(info: ProjectInfo) extends DefaultWebProject(info)
 {
-  val lift = "net.liftweb" % "lift-core" % "1.1-M8" % "compile->default"
-  val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default"
-  val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided->default"
-  val derby = "org.apache.derby" % "derby" % "10.5.3.0_1" % "runtime->default"
-  val specs = "org.scala-tools.testing" % "specs" % "1.6.1" % "test->default"
+  val liftVersion = "2.0-M3"
+  val liftWebkit = "net.liftweb" % "lift-webkit" % liftVersion % "compile"
+  val liftMapper = "net.liftweb" % "lift-mapper" % liftVersion % "compile"
+  val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.22" % "test"
+  val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided"
+  val derby = "org.apache.derby" % "derby" % "10.5.3.0_1" % "runtime"
+  val specs = "org.scala-tools.testing" % "specs" % "1.6.1" % "test"
 
   // required because Ivy doesn't pull repositories from poms
   val smackRepo = "m2-repository-smack" at "http://maven.reucon.com/public"
